c6646202f0bc5c37fe87f246d29908356bcefd89
Frost <samuelteodoziolima2010@gmail.com>
2026-01-17 14:00:43 -0300

    Update build.sh

--- setup/build.sh
+++ setup/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Installing the libraries..."
+echo "Installing the libraries"
 
-haxelib install openfl 9.4.1 --quiet
 haxelib install flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
@@ -17,6 +16,7 @@ haxelib install SScript 8.1.6 --quiet
 haxelib install format 3.7.0 --quiet
 haxelib install hxp 1.3.0 --quiet
 
+haxelib git openfl https://github.com/TheFrost72/openfl --quiet
 haxelib git lime https://github.com/TheFrost72/lime --quiet
 haxelib git hxluajit https://github.com/MAJigsaw77/hxluajit --quiet
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git --quiet
