f22bc42d7338a26e67fac4899bdbbd4d4bac691b
Frost <samuelteodoziolima2010@gmail.com>
2026-01-17 16:03:24 -0300

    Update build.sh

--- setup/build.sh
+++ setup/build.sh
@@ -3,7 +3,8 @@ set -e
 
 echo "Installing the libraries"
 
-haxelib install openfl 9.5.0 --quiet
+haxelib install lime 8.2.2 --quiet
+haxelib install openfl 9.4.1 --quiet
 haxelib install flixel 5.6.1 --quiet
 haxelib install flixel-addons 3.2.2 --quiet
 haxelib install flixel-tools 1.5.1 --quiet
@@ -18,7 +19,6 @@ haxelib install format 3.7.0 --quiet
 haxelib install hxp 1.3.0 --quiet
 
 haxelib git hxluajit https://github.com/MAJigsaw77/hxluajit --quiet
-haxelib git lime https://github.com/openfl/lime.git --quiet
 haxelib git extension-androidtools https://github.com/TheFrost72/extension-androidtools --quiet
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate --quiet
