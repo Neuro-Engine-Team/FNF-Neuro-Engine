4400e4bccd9abdeff2caa6a75735b245865291d9
Frost <samuelteodoziolima2010@gmail.com>
2026-01-21 16:53:45 -0300

    Create WarningState.hx

---
+++ source/states/WarningState.hx
@@ -0,0 +1,11 @@
+package states;
+
+import flixel.FlxState;
+import flixel.FlxG;
+import objects.Alphabet;
+import flixel.util.FlxTimer;
+
+class WarningState extends FlxState
+{
+  override function create()
+}
