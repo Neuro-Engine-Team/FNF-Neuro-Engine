415f145f3588ee74137984a0a445673f32c4adbd
Frost <samuelteodoziolima2010@gmail.com>
2026-01-21 20:26:52 -0300

    Update WarningState.hx

--- source/states/WarningState.hx
+++ source/states/WarningState.hx
@@ -4,8 +4,19 @@ import flixel.FlxState;
 import flixel.FlxG;
 import objects.Alphabet;
 import flixel.util.FlxTimer;
+import flixel.tweens.FlxTween 
 
 class WarningState extends FlxState
 {
   override function create()
+  {
+    super.create();
+    var warningtext:Alphabet = new Alphabet(0, 0, "This mod uses the Psych Engine code");
+    warningtext.screenCenter();
+    add(warningtext);
+    new FlxTimer().start(3, function(timer:FlxTimer)
+                         {
+                           remove(warningtext);
+                         });
+  }
 }
