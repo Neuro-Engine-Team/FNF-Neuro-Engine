4bcdb004cca5a46015045d059670bb2b39200b7a
Frost <samuelteodoziolima2010@gmail.com>
2026-01-22 16:13:31 -0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -148,22 +148,10 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
 		#if mobile
-        FlxG.signals.postGameStart.addOnce(() -> {
-			var applyFill = function(w:Int, h:Int):Void {
-		    var scale:Float = Math.max(w / FlxG.width, h / FlxG.height);
-			for (cam in FlxG.cameras.list) {
-            if (cam == null) continue;
-            cam.flashSprite.scaleX = scale;
-            cam.flashSprite.scaleY = scale;
-            cam.setScale(scale, scale);
-        }
-        };
-        FlxG.signals.gameResized.add(function(w,h) {
-			applyFill(w,h);
+		FlxG.signals.postGameStart.addOnce(() -> {
+			FlxG.scaleMode = new MobileScaleMode();
 		});
-        applyFill(Std.int(Lib.current.stage.stageWidth), Std.int(Lib.current.stage.stageHeight));
-		});
-	    #end
+		#end
 		addChild(new FlxGame(game.width, game.height, #if COPYSTATE_ALLOWED !CopyState.checkExistingFiles() ? CopyState : #end game.initialState, game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
