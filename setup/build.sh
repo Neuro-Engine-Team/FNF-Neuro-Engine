689d12d6da44d83535a765649978095a24e69808
Frost <samuelteodoziolima2010@gmail.com>
2026-01-24 17:50:33 -0300

    Update build.sh

--- setup/build.sh
+++ setup/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Installing the libraries"
+cd ..
+echo "Makking the main haxelib and setuping folder in same time.."
+mkdir ~/haxelib && haxelib setup ~/haxelib
+echo "Installing dependencies..."
+echo "This might take a few moments depending on your internet speed."
 haxelib install hxcpp 4.3.2 --skip-dependencies --quiet
 haxelib install lime 8.1.2 --skip-dependencies --quiet
 haxelib install openfl 9.3.3 --skip-dependencies --quiet
@@ -19,7 +23,7 @@ haxelib install hscript-iris 1.1.3 --skip-dependencies --quiet
 haxelib install SScript 8.1.6 --skip-dependencies --quiet
 
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git --skip-dependencies --quiet
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate --skip-dependencies --quiet
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 haxelib git extension-androidtools https://github.com/Neuro-Engine-Team/extension-androidtools --skip-dependencies --quiet
